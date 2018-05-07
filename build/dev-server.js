06ae93a74a56b5d5ef86b4c43199b7881955ab9e
黄正 <zheng.huang@flaginfo.com.cn>
2018-05-07 10:32:47 +0800

    webpack 4 reload everytime

--- build/dev-server.js
+++ build/dev-server.js
@@ -39,13 +39,14 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
     log: () => {}
 })
+/* webpack 4 reload everytime
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
     compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
         hotMiddleware.publish({action: 'reload'})
         cb()
     })
-})
+})*/
 
 // proxy api requests
 // Object.keys(proxyTable).forEach(function (context) {
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -37,7 +37,7 @@ module.exports = {
             },*/
             {
                 test: /\.vue$/,
-                use: 'vue-loader',
+                loader: 'vue-loader',
                 options: vueLoaderConfig
             },
             {
