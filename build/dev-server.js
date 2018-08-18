ed1e4ed1ea8ef4e835d9f9b3459fbde5f7d332e9
黄正 <zheng.huang@flaginfo.com.cn>
2018-08-18 11:45:09 +0800

    升级

--- build/dev-server.js
+++ build/dev-server.js
@@ -29,7 +29,7 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 var proxyTable = config.dev.proxyTable
 
 var app = express()
-var compiler = webpack(webpackConfig)
+var compiler = webpack(webpackConfig);
 
 //进度
 var readline = require('readline');
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -75,4 +75,17 @@ module.exports = {
     plugins: [
         new VueLoaderPlugin()
     ],
+
+    node: {
+        // prevent webpack from injecting useless setImmediate polyfill because Vue
+        // source contains it (although only uses it if it's native).
+        setImmediate: false,
+        // prevent webpack from injecting mocks to Node native modules
+        // that does not make sense for the client
+        dgram: 'empty',
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+        child_process: 'empty'
+    }
 }
