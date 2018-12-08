4474919cf6c6e14a3e9bcc245e3b2a0e85b1f8d8
h.zheng <zheng.huang@flaginfo.com.cn>
2018-12-08 18:22:58 +0800

    title

--- config/app-config.js
+++ config/app-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     LOGIN_PATH: './',
-    title: '信息服务平台',
-    description: '信息服务平台',
+    title: 'vue + webpack4 + element-ui脚手架项目',
+    description: 'vue + webpack4 + element-ui脚手架项目',
 
     themeColor: '#f67a17',
     themeFile: 'css/theme-colors.css',
