c71b3dd13733e23a88aadcda153288d8367bd187
h.zheng <zheng.huang@flaginfo.com.cn>
2019-01-12 18:22:54 +0800

    实现将本地80端口代理到9998端口，配合hosts配置，用于调试微信（因为微信必须使用域名）

---
+++ mock/proxy80.js
@@ -0,0 +1,11 @@
+// 实现将本地80端口代理到9998端口，配合hosts配置，用于调试微信（因为微信必须使用域名）
+
+const config = {
+  mockEnabled: false,
+  proxyTarget: function(urlPart) {
+    return 'http://localhost:9998'
+  },
+  isHttps: false, // 是否https
+  port: 80 // 端口
+}
+module.exports = config
