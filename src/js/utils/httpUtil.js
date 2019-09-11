ec2185479615b19b912ebcd38d42cbdf34e6c7e9
huangzheng <zheng.huang@flaginfo.com.cn>
2019-09-11 10:27:31 +0800

    delete method

--- src/js/utils/httpUtil.js
+++ src/js/utils/httpUtil.js
@@ -3,18 +3,27 @@ import axios from '../axios';
 var httpUtil = {
     axios,
     get(url, pars, config) {
-        if (pars) {
-            pars.f_rnd = +new Date(); //防止火狐缓存GET请求
-            url += url.indexOf('?') > -1 ? '&' : '?';
-            url += Object.keys(pars).map(key => key + '=' + encodeURIComponent(pars[key])).join('&');
-        }
-        return axios.get(url, config)
+        return axios.get(toQueryUrl(url, pars), config)
     },
 };
-['post', 'put', 'delete'].map(method => {
+
+['post', 'put'].map(method => {
     httpUtil[method] = (url, pars, config) => {
         return axios[method](url, pars, config)
     }
 })
+httpUtil.delete = (url, pars, config) => {
+    return axios.delete(url, pars && { data: pars }, config)
+}
+httpUtil.toQueryUrl = toQueryUrl;
 
 export default httpUtil;
+
+function toQueryUrl(rawUrl, pars) {
+    if (pars) {
+        pars.f_rnd = +new Date(); //防止火狐缓存GET请求
+        rawUrl += rawUrl.indexOf('?') > -1 ? '&' : '?';
+        rawUrl += Object.keys(pars).map(key => key + '=' + encodeURIComponent(pars[key])).join('&');
+    }
+    return rawUrl
+};
