fddd6d741510689b314ac77741ad6d51394d5801
hz <hz932@qq.com>
2021-03-25 13:25:49 +0800

    Update webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -67,7 +67,8 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit: 10000,
-                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+                    esModule: false
                 }
             },
             {
