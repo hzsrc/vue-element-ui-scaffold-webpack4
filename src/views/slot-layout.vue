f1b08da35689d65dbc8dc173cb661e4135c90bc7
hz <hz932@qq.com>
2021-08-15 20:14:37 +0800

    引入slot-layout样例

---
+++ src/views/slot-layout.vue
@@ -0,0 +1,13 @@
+<template>
+    <slot-layout-sample class="full"></slot-layout-sample>
+</template>
+
+<script>
+    import SlotLayoutSample from 'slot-layout/src/sample.vue'
+
+    export default {
+        components: {
+            SlotLayoutSample
+        }
+    }
+</script>
